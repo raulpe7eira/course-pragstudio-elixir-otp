1787b535f357da4d97f0930c33a29f16c1fcef4a
Raul Pereira <raul.pereira@gmail.com>
2022-08-15 12:58:52 -0300

    step 12: modeling with structs

--- servy/lib/servy/file_handler.ex
+++ servy/lib/servy/file_handler.ex
@@ -1,13 +1,16 @@
 defmodule Servy.FileHandler do
-  def handle_file({:ok, content}, conv) do
+
+  alias Servy.Conv
+
+  def handle_file({:ok, content}, %Conv{} = conv) do
     %{ conv | status: 200, resp_body: content }
   end
 
-  def handle_file({:error, :enoent}, conv) do
+  def handle_file({:error, :enoent}, %Conv{} = conv) do
     %{ conv | status: 404, resp_body: "File not found!" }
   end
 
-  def handle_file({:error, reason}, conv) do
+  def handle_file({:error, reason}, %Conv{} = conv) do
     %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 end
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -5,6 +5,8 @@ defmodule Servy.Handler do
   import Servy.Parser, only: [parse: 1]
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, emojify: 1, track: 1]
 
+  alias Servy.Conv
+
   @pages_path Path.expand("../../pages", __DIR__)
 
   @doc "Transforms the request into a response."
@@ -19,61 +21,49 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(%{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(%{ method: "GET", path: "/bears/new" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
     route(%{ conv | path: "/pages/form" })
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(%{ method: "DELETE", path: "/bears/" <> _id } = conv) do
+  def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
-  def route(%{ method: "GET", path: "/about" } = conv) do
+  def route(%Conv{ method: "GET", path: "/about" } = conv) do
     route(%{ conv | path: "/pages/about" })
   end
 
-  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+  def route(%Conv{method: "GET", path: "/pages/" <> file} = conv) do
     @pages_path
     |> Path.join(file <> ".html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ path: path } = conv) do
+  def route(%Conv{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    codes = %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }
-    codes[code]
   end
 end
 
