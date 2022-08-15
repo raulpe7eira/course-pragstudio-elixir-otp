70f52f20b715ac89a24cfa7797c32ec3e4b50f93
Raul Pereira <raul.pereira@gmail.com>
2022-08-15 09:39:50 -0300

    step 05: immutable data

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -3,7 +3,6 @@ defmodule Servy.Handler do
     # conv = parse(request)
     # conv = route(conv)
     # format_response(conv)
-
     request
     |> parse
     |> route
@@ -21,17 +20,16 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
   def format_response(conv) do
-
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
