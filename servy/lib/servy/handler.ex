eea1db6c024bd311e45210add9871a1235f5c6f3
Raul Pereira <raul.pereira@gmail.com>
2022-08-15 10:52:48 -0300

    step 08: pattern matching maps

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -2,8 +2,10 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
 
@@ -14,39 +16,43 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{
-      method: method,
-      path: path,
-      resp_body: "",
-      status: nil
-    }
+    %{ method: method, path: path, resp_body: "", status: nil }
   end
+
+  def rewrite_path(%{ path: "/wildlife" } = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
-
-  def route(conv, "GET",  "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, "DELETE", "/bears/" <> _id) do
+  def route(%{ method: "DELETE", path: "/bears/" <> _id } = conv) do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
+
+  def track(%{ status: 404, path: path } = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
 
   def format_response(conv) do
     """
@@ -59,14 +65,15 @@ defmodule Servy.Handler do
   end
 
   defp status_reason(code) do
-    %{
+    codes = %{
       200 => "OK",
       201 => "Created",
       401 => "Unauthorized",
       403 => "Forbidden",
       404 => "Not Found",
       500 => "Internal Server Error"
-    }[code]
+    }
+    codes[code]
   end
 end
 
@@ -116,6 +123,17 @@ IO.puts response
 
 request ="""
 DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request ="""
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
