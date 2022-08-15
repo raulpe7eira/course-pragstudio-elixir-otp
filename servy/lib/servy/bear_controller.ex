727511c98d84377a6f8301cad3c0da6623e88aa4
Raul Pereira <raul.pereira@gmail.com>
2022-08-15 16:43:53 -0300

    step 16: comprehensions

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -4,18 +4,15 @@ defmodule Servy.BearController do
   alias Servy.Conv
   alias Servy.Wildthings
 
+  @templates_path Path.expand("../../templates", __DIR__)
+
   def index(%Conv{} = conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join
 
-    %Conv{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
+    render(conv, "index.eex", bears: bears)
   end
-
-  defp bear_item(bear), do: "<li>#{bear.name} - #{bear.type}</li>"
 
   def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
     route(%Conv{ conv | path: "/pages/form" })
@@ -24,7 +21,7 @@ defmodule Servy.BearController do
   def show(%Conv{} = conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    %Conv{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>" }
+    render(conv, "show.eex", bear: bear)
   end
 
   def delete(%Conv{} = conv, %{"id" => id}) do
@@ -33,5 +30,14 @@ defmodule Servy.BearController do
 
   def create(%Conv{} = conv, %{"name" => name, "type" => type}) do
     %Conv{ conv | status: 201, resp_body: "Create a #{type} bear named #{name}!" }
+  end
+
+  defp render(%Conv{} = conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %Conv{ conv | status: 200, resp_body: content }
   end
 end
