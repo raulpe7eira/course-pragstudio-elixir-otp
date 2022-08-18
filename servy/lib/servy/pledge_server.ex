b0c3ba61f8739d43bbb5d0ea69ae18f1ae89dc57
Raul Pereira <raul.pereira@gmail.com>
2022-08-18 00:04:53 -0300

    step 24: stateful server processes - part 1

---
+++ servy/lib/servy/pledge_server.ex
@@ -0,0 +1,18 @@
+defmodule Servy.PledgeServer do
+  def create_pledge(name, amount) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+
+    # Cache the pledge
+    [ {"larry", 10} ]
+  end
+
+  def recent_pledges do
+    # Returns the most recent pledges (cache):
+    [ {"larry", 10} ]
+  end
+
+  defp send_pledge_to_service(name, amount) do
+    # CORE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
+    {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+end
