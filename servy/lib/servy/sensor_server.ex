1a0046c071dbdb35fda4e20333a3b58408d66634
Raul Pereira <raul.pereira@gmail.com>
2022-08-27 23:12:36 -0300

    step 27: another genserver (exercises)

--- servy/lib/servy/sensor_server.ex
+++ servy/lib/servy/sensor_server.ex
@@ -2,39 +2,58 @@ defmodule Servy.SensorServer do
   use GenServer
 
   @name :sensor_server
-  @refresh_interval :timer.seconds(5)
+
+  defmodule State do
+    defstruct sensor_data: %{}, refresh_interval: :timer.minutes(60)
+  end
 
   # Client Interface
 
   def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def get_sensor_data do
     GenServer.call @name, :get_sensor_data
   end
 
+  def set_refresh_interval(time_in_ms) do
+    GenServer.cast @name, {:set_refresh_interval, time_in_ms}
+  end
+
   # Server Callbacks
 
-  def init(_state) do
-    initial_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+  def init(%State{} = _state) do
+    sensor_data = run_tasks_to_get_sensor_data()
+    initial_state = %{state | sensor_data: sensor_data}
+    schedule_refresh(state.refresh_interval)
     {:ok, initial_state}
   end
 
-  def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+  def handle_call(:get_sensor_data, _from, %State{} = state) do
+    {:reply, state.sensor_data, state}
   end
 
-  def handle_info(:refresh, _state) do
-    IO.puts "Refreshing the cache..."
-    new_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+  def handle_cast({:set_refresh_interval, time_in_ms}, state) do
+    new_state = %{ state | refresh_interval: time_in_ms }
     {:noreply, new_state}
   end
 
-  defp schedule_refresh do
-    Process.send_after(self(), :refresh, @refresh_interval)
+  def handle_info(:refresh, %State{} = _state) do
+    IO.puts "Refreshing the cache..."
+    sensor_data = run_tasks_to_get_sensor_data()
+    initial_state = %{state | sensor_data: sensor_data}
+    schedule_refresh(state.refresh_interval)
+    {:ok, initial_state}
+  end
+
+  def handle_info(unexpected, %State{} = state) do
+    IO.puts "Can't touch this! #{inspect unexpected}"
+    {:noreply, state}
+  end
+
+  defp schedule_refresh(time_in_ms) do
+    Process.send_after(self(), :refresh, time_in_ms)
   end
 
   defp run_tasks_to_get_sensor_data do
